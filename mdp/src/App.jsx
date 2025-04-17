2ea1469ef1997328888c924dd79e22c84a5cd575
Marguillat <erwanduchene@gmail.com>
2025-04-17 12:15:30 +0200

    imports libs

--- mdp/src/App.jsx
+++ mdp/src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import TabBar from './components/Tabbar/Tabbar.jsx';
-import './App.css';
+import React from "react";
+import TabBar from "./components/Tabbar/Tabbar.jsx";
+import { Button } from "@mui/material";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Mon Application</h1>
-            <TabBar />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1 className="bg-red-500">Mon Application</h1>
+      <TabBar />
+      <Button>hello button</Button>
+    </div>
+  );
 }
 
 export default App;
