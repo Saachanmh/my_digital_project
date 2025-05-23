1855264fa78eaf4dabe1b5358375030e425765e2
Chloe Hervouet <chloe.dp.h@hotmail.fr>
2025-05-23 14:12:34 +0200

    protection

--- mdp/src/Router/MainRouter.jsx
+++ mdp/src/Router/MainRouter.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
 import ProfilePage from '../pages/ProfilePage'
 import ExercicePage from '../pages/ExercicePage.jsx'
 import StatistiquesPage from '../pages/StatistiquesPage.jsx'
@@ -15,10 +16,11 @@ function MainRouter () {
                 path='*'
                 element={<Navigate to='/' replace />}
             />
-            <Route path='/' element={<TrainingPage />} />
+            <Route path='/' element={<HomePage />} />
             <Route path='/profile' element={<ProfilePage />} />
             <Route path='/exercice' element={<ExercicePage/>} />
             <Route path='/stat' element={<StatistiquesPage/>} />
+            <Route path='/training' element={<TrainingPage/>} />
             <Route path='/workout-session' element={<WorkoutSessionPage/>} />
             <Route path='/workout-timer' element={<WorkoutTimerPage/>} />
             <Route path='/edit-exercise/:id' element={<EditExercisePage/>} />
--- mdp/src/components/Tabbar/Tabbar.jsx
+++ mdp/src/components/Tabbar/Tabbar.jsx
@@ -1,72 +1,33 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import './Tabbar_module.css';
 import imageAccueil from '../../assets/home.png';
+import imageCalendrier from '../../assets/calendar.png';
 import imageEntrainement from '../../assets/barbell.png';
 import imageUtilisateur from '../../assets/user.png';
 import imageAutre from '../../assets/ellipsis.png';
 
 const TabBar = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    
-    // Function to check if a route is active
-    const isActive = (path) => {
-        return location.pathname === path;
-    };
-
     return (
-        <div className="flex justify-around bg-gray-100 p-3 fixed bottom-0 w-full shadow-[0_-2px_5px_rgba(0,0,0,0.1)]">
-            <button 
-                className="flex flex-col items-center focus:outline-none"
-                onClick={() => navigate('/')}
-            >
-                <img 
-                    src={imageAccueil} 
-                    alt="Accueil" 
-                    className={`w-7 h-7 mb-1 ${isActive('/') ? 'filter brightness-75 sepia-100 hue-rotate-190 saturate-500' : ''}`}
-                />
-                <span className={`text-xs ${isActive('/') ? 'text-blue-500 font-bold' : 'text-gray-700'}`}>
-                    Home
-                </span>
+        <div className="tabbar">
+            <button className="tabbar-button">
+                <img src={imageAccueil} alt="Accueil" />
+                <span>Home</span>
             </button>
-            <button 
-                className="flex flex-col items-center focus:outline-none"
-                onClick={() => navigate('/exercice')}
-            >
-                <img 
-                    src={imageEntrainement} 
-                    alt="Entrainement" 
-                    className={`w-7 h-7 mb-1 ${isActive('/exercice') ? 'filter brightness-75 sepia-100 hue-rotate-190 saturate-500' : ''}`}
-                />
-                <span className={`text-xs ${isActive('/exercice') ? 'text-blue-500 font-bold' : 'text-gray-700'}`}>
-                    Entrainements
-                </span>
+            <button className="tabbar-button">
+                <img src={imageCalendrier} alt="Calendrier" />
+                <span>Search</span>
             </button>
-            <button 
-                className="flex flex-col items-center focus:outline-none"
-                onClick={() => navigate('/stat')}
-            >
-                <img 
-                    src={imageUtilisateur} 
-                    alt="Utilisateurs" 
-                    className={`w-7 h-7 mb-1 ${isActive('/stat') ? 'filter brightness-75 sepia-100 hue-rotate-190 saturate-500' : ''}`}
-                />
-                <span className={`text-xs ${isActive('/stat') ? 'text-blue-500 font-bold' : 'text-gray-700'}`}>
-                    Utilisateurs
-                </span>
+            <button className="tabbar-button">
+                <img src={imageEntrainement} alt="Entrainement" />
+                <span>Profile</span>
             </button>
-            <button 
-                className="flex flex-col items-center focus:outline-none"
-                onClick={() => navigate('/login')}
-            >
-                <img 
-                    src={imageAutre} 
-                    alt="Plus" 
-                    className={`w-7 h-7 mb-1 ${isActive('/login') ? 'filter brightness-75 sepia-100 hue-rotate-190 saturate-500' : ''}`}
-                />
-                <span className={`text-xs ${isActive('/login') ? 'text-blue-500 font-bold' : 'text-gray-700'}`}>
-                    Connexion
-                </span>
+            <button className="tabbar-button">
+                <img src={imageUtilisateur} alt="Utilisateurs" />
+                <span>Settings</span>
+            </button>
+            <button className="tabbar-button">
+                <img src={imageAutre} alt="Plus" />
+                <span>Settings</span>
             </button>
         </div>
     );
