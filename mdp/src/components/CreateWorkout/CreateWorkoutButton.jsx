bbd30b35236e647c675495fdbd03c67422d93e86
Chloe Hervouet <chloe.dp.h@hotmail.fr>
2025-06-08 17:40:02 +0200

    page exercice

--- mdp/src/components/CreateWorkout/CreateWorkoutButton.jsx
+++ mdp/src/components/CreateWorkout/CreateWorkoutButton.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const CreateWorkoutButton = ({ onClick }) => {
+const CreateWorkoutButton = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/create-workout');
+  };
+
   return (
     <div className="px-4 mb-5">
       <button
         className="w-full p-2 bg-gray-100 rounded-xl text-base text-center transition-colors hover:bg-gray-200 active:bg-gray-300 whitespace-nowrap"
-        onClick={onClick}
+        onClick={handleClick}
       >
         Créer une nouvelle routine d'entrainement
       </button>
