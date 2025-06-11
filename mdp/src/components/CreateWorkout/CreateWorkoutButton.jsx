d5ee2fd943f05f74b3b272534a282be8889e3c5f
marguillat <erwanduchene@gmail.com>
2025-06-11 14:38:20 +0200

    graphique workoutsession

--- mdp/src/components/CreateWorkout/CreateWorkoutButton.jsx
+++ mdp/src/components/CreateWorkout/CreateWorkoutButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const CreateWorkoutButton = () => {
+const CreateWorkoutButton = ({onClick}) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
